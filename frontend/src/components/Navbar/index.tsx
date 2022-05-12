18b450e4fdcaee8ba3f756cca3b35f6218e85d09
mateuslgomes <mateuslgomes021@gmail.com>
2022-05-12 20:40:16 -0300

    fixing bug

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -2,13 +2,12 @@ import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
 import './styles.css';
 
 function Navbar() {
-
     return (
         <header>
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/mateuslgomes">
+                    <a href="https://github.com/mateuslgomes" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/mateuslgomes</p>
